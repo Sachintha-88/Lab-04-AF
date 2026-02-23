71b4cc6d299b6665f71fd959868c81c39c6bc9ed
Sachintha Nirmal <sachinthanirmal658@gmail.com>
2026-02-23 20:32:04 +0530

    write-file done

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ fs.readFile('file.txt', 'utf8', function (err, data) {
  console.log(data);
 });
 
+console.log("write file");
+const fs = require('fs');
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+ if (err) throw err;
+ console.log('File saved!');
+});
